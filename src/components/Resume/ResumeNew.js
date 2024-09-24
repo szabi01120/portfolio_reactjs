838de84dbfbb4396690451f9c03d05c673d394e3
szabi01120 <hajnalszabolcs0@gmail.com>
2024-09-25 00:05:47 +0200

    resume added

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+import pdf from "../../Assets/../Assets/Szabolcs_Hajnal_English_CV.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -22,8 +22,7 @@ function ResumeNew() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            // href={pdf}
-            href=""
+            href={pdf}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
@@ -34,16 +33,14 @@ function ResumeNew() {
 
         <Row className="resume">
           <Document file={pdf} className="d-flex justify-content-center">
-            {/* <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} /> */}
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
-          ide jön majd a cv
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            // href={pdf}
-            href=""
+            href={pdf}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
